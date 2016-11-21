35e4c56bf06363b4f6508fbf236c17415d2c0b88
evodify <social7332@yahoo.com>
2016-11-21 17:15:16 +0100

    updated

--- find_biased_SNPs/extract_unbiasedRNA.sh
+++ find_biased_SNPs/extract_unbiasedRNA.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# to extract the RNA data SNPs that showed no bias in the DNA data.
+#
+# command:
+# 
+# sh extractUnbiased.sh DNAinput1_unbiased.csv RNAinput.csv
+
 sed 's/"//g;s/ /\t/g' $1 | cut -f 2 > $1_Names.csv
 grep -Fwf $1_Names.csv $2 > $2_unbiased.csv
 rm $1_Names.csv
