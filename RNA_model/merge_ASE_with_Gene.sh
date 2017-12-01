76ee9b90b6eddbf468b8ba90ee88fc13b6c72323
evodify <social7332@yahoo.com>
2017-12-01 19:28:46 +0100

    extended output columns

--- RNA_model/merge_ASE_with_Gene.sh
+++ RNA_model/merge_ASE_with_Gene.sh
@@ -7,8 +7,8 @@
 #
 # sh merge_ASE_with_Gene.sh RNAinput1_unbiased_allASE.csv Genes_RNAinput1.csv
 
-echo -e "" > header_temp
-sort $1 > $1_temp
-sort $2 > $2_temp
+sed 's/"//g' $1 | sort > $1_temp
+sed 's/"//g' $2 | sort > $2_temp
 join -j 1 $1_temp $2_temp | sed 's/ /\t/g;1i\gene\tASE_prob\tnumber_of_SNPs\thomeologueA_count\thomeologueB_count\tproportion_of_homeologueA' > $2_mergedASE.csv
-rm *_temp
+rm $1_temp $2_temp
+ 
--- find_biased_SNPs/merge_by_Genes.sh
+++ find_biased_SNPs/merge_by_Genes.sh
@@ -6,4 +6,8 @@
 #
 # sh merge_ASE_by_Gene.sh RNAinput1_unbiased_allASE.csv
 
-awk  'BEGIN {print "Gene\tnumber_of_SNPs\thomeologueA_count\thomeologueB_count\tproportion_of_homeologueA"}; NR>1, NF>1 {a[$1] = a[$1]+=$3; b[$1] = b[$1]+=$4; n[$1] = n[$1]+=1}; END {for (i in a) {print i"\t"n[i]"\t"a[i]"\t"b[i]"\t"(a[i]/(a[i]+b[i]))} }' $1 > Genes_$1
+# ratio per SNP averaged per gene:
+awk  'BEGIN {print "Gene\tnumber_of_SNPs\tproportion_of_homeologueA"}; NR>1, NF>1 {a[$1] += $3/($3+$4); n[$1] = n[$1]+=1}; END {for (i in a) {print i"\t"n[i]"\t"(a[i]/n[i])} }' $1 > $1.Genes
+
+# ratio per gene:
+# awk 'BEGIN {print "Gene\tnumber_of_SNPs\thomeologueA_count\thomeologueB_count\tproportion_of_homeologueA"}; NR>1, NF>1 {a[$1] = a[$1]+=$3; b[$1] = b[$1]+=$4; n[$1] = n[$1]+=1}; END {for (i in a) {print i"\t"n[i]"\t"a[i]"\t"b[i]"\t"(a[i]/(a[i]+b[i]))} }' $1 > $1.Genes
